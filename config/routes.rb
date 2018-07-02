7e5cf36e16c2e80ee37c714027cb184bc647f7ed
kennylai5198 <kennylai.kimlung@outlook.com>
2018-07-02 19:19:24 +0800

    Added a home, about us and contact us pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  # get 'pages/home'
+  root 'pages#home'
+  get 'about' => 'pages#about'
+  get 'contact_us' => 'pages#contact_us'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
