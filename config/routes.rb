46e45c31f532e8be3aff5f3363269a0a05b0feca
kennylai5198 <kennylai.kimlung@outlook.com>
2018-08-01 21:36:33 +0800

    Created User Profiles

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
+  get 'users/:username', to: 'users#show', as: 'user'
+
   resources :items
   resources :tweets
   ActiveAdmin.routes(self)
   devise_for :users
   as :user do
-    get "signin" => 'devise/sessions#new'
-    delete "signout" => 'devise/sessions#destroy'
-    get "signup" => 'devise/registrations#new'
+    get "signin", to: 'devise/sessions#new'
+    delete "signout", to: 'devise/sessions#destroy'
+    get "signup", to: 'devise/registrations#new'
   end
   # get 'pages/home'
   root 'pages#home'
-  get 'about' => 'pages#about'
-  get 'contact_us' => 'pages#contact_us'
+  get 'about', to: 'pages#about'
+  get 'contact_us', to: 'pages#contact_us'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
